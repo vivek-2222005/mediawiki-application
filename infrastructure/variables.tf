e505a91c6a323d4db2322f13bf0aae182539e172
Vivek Mishra <vivek_2222005@yahoo.co.in>
2024-03-11 11:51:34 +0530

    formatting

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,15 +1,18 @@
 variable "prefix" {
   description = "A prefix used for all resources in this example"
+  type        = string
   default     = "mediawiki"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be provisioned"
+  type        = string
   default     = "eastus"
 }
 
 variable "env" {
   description = "Possible values could be Dev/Test/Prod."
+  type        = string
   default     = "dev"
 }
 
